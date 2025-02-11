9c0664f6d0eadca13bb9bf807cc76aa9bac89aa8
Andrea Fastame <a.fastame@gmail.com>
2025-02-11 15:03:11 +0100

    Refactor 1

--- src/main/java/am/markov/orderbook/model/OrderBook.java
+++ src/main/java/am/markov/orderbook/model/OrderBook.java
@@ -2,9 +2,7 @@ package am.markov.orderbook.model;
 
 import am.markov.orderbook.pojo.Order;
 
-import java.util.Comparator;
-import java.util.Hashtable;
-import java.util.TreeMap;
+import java.util.*;
 
 public class OrderBook {
 
@@ -22,16 +20,6 @@ public class OrderBook {
      * to retrieve the interested Order with direct-access (by orderId)
      */
     public Hashtable<Long, Order> ordersIndex = new Hashtable<>();
-    /**
-     * Indexed hashtables containing Buy and Sell orders, used to understand if an EDIT Actions is requested for a Buy or
-     * for a Sell Order
-     */
-    // TODO [LV]: You don't need them at all. You can get from the orderIndex, as they use the same key with direct access.
-    //  You can read the buy / sell from the original order object.
-    //  TODO [AF]: I thought about it, then i Realized that maybe with the isBuy() property I could completely refactor in order to avoid using / managing them
-    public Hashtable<Long, Order> buyOrdersIndex = new Hashtable<>();
-    public Hashtable<Long, Order> sellOrdersIndex = new Hashtable<>();
-
 
     public OrderBook(String symbol) {
         this.symbol = symbol;
@@ -48,19 +36,11 @@ public class OrderBook {
             // TODO [LV]: The method put in a Java map is adding the element if it doesn't exist or override the value if exists with the same key.
             // TIPS: What about using put method with getOrDefault on actual quantity, and add the order quantity?
             // TODO [AF]: Maybe i misunderstood the mechanics, which i implied of not just "overriding" the value contained @ the price index, but recalculate. If it's just an override, it is even easier
-            if (buyOpsQuantitiesIndexedByPrice.containsKey(order.getPrice())) {
-                buyOpsQuantitiesIndexedByPrice.compute(order.getPrice(), (k, oldQuantity) -> oldQuantity + order.getQuantity());
-            } else {
-                buyOpsQuantitiesIndexedByPrice.put(order.getPrice(), order.getQuantity());
-            }
-            buyOrdersIndex.put(order.getOrderId(), order);
+            int newQuantity = buyOpsQuantitiesIndexedByPrice.getOrDefault(order.getPrice(), 0) + order.getQuantity();
+            buyOpsQuantitiesIndexedByPrice.put(order.getPrice(), newQuantity);
         } else {
-            if (sellOpsQuantitiesIndexedByPrice.containsKey(order.getPrice())) {
-                sellOpsQuantitiesIndexedByPrice.compute(order.getPrice(), (k, oldQuantity) -> oldQuantity + order.getQuantity());
-            } else {
-                sellOpsQuantitiesIndexedByPrice.put(order.getPrice(), order.getQuantity());
-            }
-            sellOrdersIndex.put(order.getOrderId(), order);
+            int newQuantity = sellOpsQuantitiesIndexedByPrice.getOrDefault(order.getPrice(), 0) + order.getQuantity();
+            sellOpsQuantitiesIndexedByPrice.put(order.getPrice(), newQuantity);
         }
         ordersIndex.put(order.getOrderId(), order);
     }
@@ -68,15 +48,14 @@ public class OrderBook {
     public Boolean edit(Order order) {
         Order orderToChange = ordersIndex.get(order.getOrderId());
         if (orderToChange != null) {
-            if (buyOrdersIndex.containsKey(order.getOrderId())) {
+            if (orderToChange.isBuy()) {
                 // Buy order
                 // changing qty in aggregates
                 computeNewQuantity(buyOpsQuantitiesIndexedByPrice, orderToChange, order);
-            } else if (sellOrdersIndex.containsKey(order.getOrderId())) {
+            } else {
                 // Sell order
                 // changing qty in aggregates
                 computeNewQuantity(sellOpsQuantitiesIndexedByPrice, orderToChange, order);
-
             }
             return true;
         } else
@@ -85,9 +64,13 @@ public class OrderBook {
 
     // TODO [LV]: What about using remove and add instead of re-writing it?
     // TODO [AF]: It's another way of handling it, it is actually a cleaner method of doing it without any hassle of double checking for existance. Will do in refactoring
+    // TODO [AF]: I actually have not very clear if the value just must be updated with new one or must be recalculated (as I did)
     private void computeNewQuantity(TreeMap<Integer, Integer> opsQuantitiesIndexedByPrice, Order oldIndexedOrder, Order newOrder) {
 
+        // Subtract old quantity, if present, from the previous indexed order
         opsQuantitiesIndexedByPrice.computeIfPresent(oldIndexedOrder.getPrice(), (k, oldQuantity) -> oldQuantity - oldIndexedOrder.getQuantity());
+
+        // Remove if quantity == 0
         if (opsQuantitiesIndexedByPrice.get(oldIndexedOrder.getPrice()) <= 0) {
             opsQuantitiesIndexedByPrice.remove(oldIndexedOrder.getPrice());
         }
@@ -97,39 +80,39 @@ public class OrderBook {
         oldIndexedOrder.setQuantity(newOrder.getQuantity());
 
         // If contained we need to sum the new value to the old one, otherwise we'll just set a new <key,value> tuple
-        if (opsQuantitiesIndexedByPrice.containsKey(newOrder.getPrice())) {
-            opsQuantitiesIndexedByPrice.compute(oldIndexedOrder.getPrice(), (k, oldQuantity) -> oldQuantity + oldIndexedOrder.getQuantity());
-        } else {
-            opsQuantitiesIndexedByPrice.put(oldIndexedOrder.getPrice(), oldIndexedOrder.getQuantity());
-        }
+        int newQuantity = opsQuantitiesIndexedByPrice.getOrDefault(oldIndexedOrder.getPrice(), 0) + oldIndexedOrder.getQuantity();
+        opsQuantitiesIndexedByPrice.put(newOrder.getPrice(), newQuantity);
+//        if (opsQuantitiesIndexedByPrice.containsKey(newOrder.getPrice())) {
+//            opsQuantitiesIndexedByPrice.compute(oldIndexedOrder.getPrice(), (k, oldQuantity) -> oldQuantity + oldIndexedOrder.getQuantity());
+//        } else {
+//            opsQuantitiesIndexedByPrice.put(oldIndexedOrder.getPrice(), oldIndexedOrder.getQuantity());
+//        }
         if (opsQuantitiesIndexedByPrice.get(oldIndexedOrder.getPrice()) <= 0) {
             opsQuantitiesIndexedByPrice.remove(oldIndexedOrder.getPrice());
         }
     }
 
     public boolean remove(Order order) {
-        boolean isBuy = buyOrdersIndex.containsKey(order.getOrderId());
         Order orderToChange = ordersIndex.get(order.getOrderId());
         if (orderToChange != null) {
             // TODO [LV]: Same as for the add method: use a function for buy / sell as they are the same
             // TODO [AF]: INDEED, I would have been done, as specified earlier, for EDIT operations, since it unclutters code
-            if (isBuy) {
-                buyOpsQuantitiesIndexedByPrice.compute(orderToChange.getPrice(), (k, oldQuantity) -> oldQuantity - orderToChange.getQuantity());
-                buyOrdersIndex.remove(order.getOrderId());
-                if (buyOpsQuantitiesIndexedByPrice.get(orderToChange.getPrice()) <= 0) {
-                    buyOpsQuantitiesIndexedByPrice.remove(orderToChange.getPrice());
-                }
+            if (orderToChange.isBuy()) {
+                doRemoveFromStructure(buyOpsQuantitiesIndexedByPrice, orderToChange);
             } else {
-                sellOpsQuantitiesIndexedByPrice.compute(orderToChange.getPrice(), (k, oldQuantity) -> oldQuantity - orderToChange.getQuantity());
-                sellOrdersIndex.remove(order.getOrderId());
-                if (sellOpsQuantitiesIndexedByPrice.get(orderToChange.getPrice()) <= 0) {
-                    sellOpsQuantitiesIndexedByPrice.remove(orderToChange.getPrice());
-                }
+                doRemoveFromStructure(sellOpsQuantitiesIndexedByPrice, orderToChange);
             }
             ordersIndex.remove(order.getOrderId());
             return true;
         } else
             return false;
+    }
+
+    private void doRemoveFromStructure(TreeMap<Integer, Integer> buyOpsQuantitiesIndexedByPrice, Order orderToChange) {
+        buyOpsQuantitiesIndexedByPrice.compute(orderToChange.getPrice(), (k, oldQuantity) -> oldQuantity - orderToChange.getQuantity());
+        if (buyOpsQuantitiesIndexedByPrice.get(orderToChange.getPrice()) <= 0) {
+            buyOpsQuantitiesIndexedByPrice.remove(orderToChange.getPrice());
+        }
     }
 
     public void print() {
